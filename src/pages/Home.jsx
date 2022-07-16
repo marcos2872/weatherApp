a43dc66c125f5dfd9ca435409d79bfba89066984
Marcos souza brito <njsu5@hotmail.com>
2022-07-16 14:30:22 -0300

    Eslint Airbnb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import search from '../services/getApiWeather';
-
 
 export default function Home() {
   const [city, setCity] = useState('');
   const [data, setData] = useState(undefined);
-  const [btnlook, setBtnlook] = useState(true)
+  const [btnlook, setBtnlook] = useState(true);
+
+  useEffect(() => {
+    // async function api() {
+    //   // & appid=b40d105652572479bed6fab2551755d2
+    //   const url = 'http://api.openweathermap.org/geo/1.0/direct?q=Timoteo&limit=1&appid=b40d105652572479bed6fab2551755d2'
+    //   const renponse = await fetch(url);
+    //   const data = await renponse.json();
+    //   console.log(data);
+    // }
+    // api();
+  }, []);
 
   const saveInput = ({ target }) => {
     const { value } = target;
@@ -13,14 +23,7 @@ export default function Home() {
     setBtnlook(value.length < 3);
   };
 
-  const btnBuscar = () => {
-    weather();
-  };
-  const keyPress = (event) => {
-    if (event.key === 'Enter') return weather();
-  }
-
- const weather = async () => {
+  const weather = async () => {
     const response = await search(city, 'metric', 'pt_br');
     console.log(response);
     setData(response);
@@ -28,27 +31,33 @@ export default function Home() {
     setBtnlook(true);
   };
 
-  return(
+  const keyPress = (event) => event.key === 'Enter' && weather();
+
+  const btnBuscar = () => {
+    weather();
+  };
+
+  return (
     <div>
       <h1 className="text-3xl font-bold underline">Weather</h1>
-         <input
-           type="text"
-           value={city}
-           name="city"
-           onChange={saveInput}
-           onKeyUp={keyPress}
-           placeholder="Digite o nome da cidade"
-         />
-         <button
-           id="pesquisa"
-           type="button"
-           placeholder="Buscar"
-           disabled={btnlook}
-           onClick={btnBuscar}
-         >
-          Buscar
-         </button>
-         {data
+      <input
+        type="text"
+        value={city}
+        name="city"
+        onChange={saveInput}
+        onKeyUp={keyPress}
+        placeholder="Digite o nome da cidade"
+      />
+      <button
+        id="pesquisa"
+        type="button"
+        placeholder="Buscar"
+        disabled={btnlook}
+        onClick={btnBuscar}
+      >
+        Buscar
+      </button>
+      {data
          && (
          <>
            <p />
@@ -59,6 +68,6 @@ export default function Home() {
            </p>
          </>
          )}
-       </div>
-  )
-};
+    </div>
+  );
+}
