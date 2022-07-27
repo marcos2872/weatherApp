9faca58efce65b63aecc5ccab3e7f10515f21cc7
danielrubens <danielrubensdaniel@gmail.com>
2022-07-27 13:26:44 -0300

    01-Iniciando Teste

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -12,9 +12,24 @@ import {
 // import renderWithRouter from '../renderWithRouter';
 
 describe('teste', () => {
-  test('renders learn react link', () => {
+  it('renders learn react link', () => {
     render(<App />, { wrapper: BrowserRouter });
     const linkElement = screen.getByText(/Weather/i);
     expect(linkElement).toBeInTheDocument();
+  });
+  it('verifica se tem botão', () => {
+    const { getByRole } = render(<App />, { wrapper: BrowserRouter });
+    const button = getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+  it('botão desabilitado', () => {
+    const { getByRole } = render(<App />, { wrapper: BrowserRouter });
+    const button = getByRole('button');
+    expect(button).toBeDisabled();
+  });
+  it('placeholder "digite o nome da cidade"', () => {
+    const { getByPlaceholderText } = render(<App />, { wrapper: BrowserRouter });
+    const placeholder = getByPlaceholderText(/digite o nome da cidade/i);
+    expect(placeholder).toBeInTheDocument();
   });
 });
